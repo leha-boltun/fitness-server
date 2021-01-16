aee5f4ba295409e70a83077d63ae41b432a8125e
leha-boltun <leha-boltun@users.noreply.github.com>
2021-01-16 17:53:09 +0700

    Fix bug - set id

--- web/src/main/java/ru/fitness/dao/TimeStampRepository.java
+++ web/src/main/java/ru/fitness/dao/TimeStampRepository.java
@@ -10,9 +10,9 @@ public interface TimeStampRepository extends Repository<TimeStamp, Long> {
     @Query("select e from TimeStamp e where e.workout.id = :workoutId order by e.wtime desc, e.id desc")
     List<TimeStamp> findByWorkoutId(long workoutId);
 
-    @Query("select t from TimeStamp t join t.eventType e join t.workout w order by e.eventOrder")
+    @Query("select t from TimeStamp t join t.eventType e join t.workout w where w.id = :workoutId order by e.eventOrder")
     List<TimeStamp> getFirstTimeStamp(long workoutId, Pageable pageable);
 
-    @Query("select t from TimeStamp t join t.eventType e join t.workout w order by e.eventOrder desc")
+    @Query("select t from TimeStamp t join t.eventType e join t.workout w where w.id = :workoutId order by e.eventOrder desc")
     List<TimeStamp> getLastTimeStamp(long workoutId, Pageable pageable);
 }
